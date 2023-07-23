a68e4baecabd6bfc76f531d89b8bc20e58b9fb71
Kailash1233 <kailash61203@gmail.com>
2023-07-23 15:18:13 +0530

    Updates

---
+++ precache-manifest.2c0ce1e085474d8bb5967122d77c9286.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ce30e2639fa72c6b5fa66e51b7d92a",
+    "url": "/ecommerce-flipkart-clone-master/index.html"
+  },
+  {
+    "revision": "f2154ffbf644ebc695f9",
+    "url": "/ecommerce-flipkart-clone-master/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "5a6409ecfe765139fb4e",
+    "url": "/ecommerce-flipkart-clone-master/static/css/main.314983d0.chunk.css"
+  },
+  {
+    "revision": "f2154ffbf644ebc695f9",
+    "url": "/ecommerce-flipkart-clone-master/static/js/2.4a2bda21.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ecommerce-flipkart-clone-master/static/js/2.4a2bda21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a6409ecfe765139fb4e",
+    "url": "/ecommerce-flipkart-clone-master/static/js/main.afe3ecb2.chunk.js"
+  },
+  {
+    "revision": "74c356a372f406d98045",
+    "url": "/ecommerce-flipkart-clone-master/static/js/runtime-main.3f9d2d25.js"
+  }
+]);
